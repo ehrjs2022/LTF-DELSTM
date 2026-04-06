ad73f5d63840b02f9d119e60a11762a5fd3a98b8
ehrjs2022 <ehrjs2022@gmail.com>
2026-04-06 12:37:11 +0900

    Add files via upload

---
+++ Turbidity/SaaS_WQ/__init__.py
@@ -0,0 +1,15 @@
+"""
+SaaS_WQ
+"""
+
+# Codes fof SaaS water quality prediction, created by Water Environmental Management Lab, UOS
+# 
+
+from . import model, model_train, preprocessing
+
+__all__ = [
+    "model",
+    "model_train",
+    "preprocessing",
+    "plot"
+]
